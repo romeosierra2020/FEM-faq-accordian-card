02d4b8d4f05fa3b7eac063102af4e940815151bd
Romeo Sierra <richard.powell.henshaws@gmail.com>
2021-02-18 13:52:09 +0000

    Mobile layout complete

---
+++ index.js
@@ -0,0 +1,79 @@
+const pointer1 = document.getElementById("pointer1");
+const question1 = document.getElementById("question1");
+const answer1 = document.getElementById("answer1");
+question1.addEventListener('click', function() {
+    clicked1();
+});
+
+function clicked1() {
+    pointer1.classList.toggle('pointer');
+    pointer1.classList.toggle('pointer-open');
+    question1.classList.toggle('question');
+    question1.classList.toggle('question-open');
+    answer1.classList.toggle('answer');
+    answer1.classList.toggle('answer-open');
+}
+
+const pointer2 = document.getElementById("pointer2");
+const question2 = document.getElementById("question2");
+const answer2 = document.getElementById("answer2");
+question2.addEventListener('click', function() {
+    clicked2();
+});
+
+function clicked2() {
+    pointer2.classList.toggle('pointer');
+    pointer2.classList.toggle('pointer-open');
+    question2.classList.toggle('question');
+    question2.classList.toggle('question-open');
+    answer2.classList.toggle('answer');
+    answer2.classList.toggle('answer-open');
+}
+
+const pointer3 = document.getElementById("pointer3");
+const question3 = document.getElementById("question3");
+const answer3 = document.getElementById("answer3");
+question3.addEventListener('click', function() {
+    clicked3();
+});
+
+function clicked3() {
+    pointer3.classList.toggle('pointer');
+    pointer3.classList.toggle('pointer-open');
+    question3.classList.toggle('question');
+    question3.classList.toggle('question-open');
+    answer3.classList.toggle('answer');
+    answer3.classList.toggle('answer-open');
+}
+
+const pointer4 = document.getElementById("pointer4");
+const question4 = document.getElementById("question4");
+const answer4 = document.getElementById("answer4");
+question4.addEventListener('click', function() {
+    clicked4();
+});
+
+function clicked4() {
+    pointer4.classList.toggle('pointer');
+    pointer4.classList.toggle('pointer-open');
+    question4.classList.toggle('question');
+    question4.classList.toggle('question-open');
+    answer4.classList.toggle('answer');
+    answer4.classList.toggle('answer-open');
+}
+
+const pointer5 = document.getElementById("pointer5");
+const question5 = document.getElementById("question5");
+const answer5 = document.getElementById("answer5");
+question5.addEventListener('click', function() {
+    clicked5();
+});
+
+function clicked5() {
+    pointer5.classList.toggle('pointer');
+    pointer5.classList.toggle('pointer-open');
+    question5.classList.toggle('question');
+    question5.classList.toggle('question-open');
+    answer5.classList.toggle('answer');
+    answer5.classList.toggle('answer-open');
+}
